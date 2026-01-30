742abc04ffdb4a07c2e2a77bdc24e649e2baa4b5
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-01-30 18:50:21 -0300

    fix: cors error fix

--- src/main/java/com/danieloliveira/file_converter/document/controller/DocumentConverterController.java
+++ src/main/java/com/danieloliveira/file_converter/document/controller/DocumentConverterController.java
@@ -16,10 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -27,6 +24,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/conversions/documents")
+@CrossOrigin
 @RequiredArgsConstructor
 @Tag(name = "Document Conversion", description = "Endpoints for converting office documents to different formats.")
 public class DocumentConverterController {
--- src/main/java/com/danieloliveira/file_converter/image/controller/ImageConverterController.java
+++ src/main/java/com/danieloliveira/file_converter/image/controller/ImageConverterController.java
@@ -15,16 +15,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/conversions/images")
+@CrossOrigin
 @RequiredArgsConstructor
 @Tag(name = "Image Conversion", description = "Endpoints for converting image files (JPEG, PNG, WEBP, etc.) to different formats.")
 public class ImageConverterController {
